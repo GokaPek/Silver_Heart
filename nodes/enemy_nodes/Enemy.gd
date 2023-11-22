45951de1620b506289a1cd73bf16294ce7971bca
GokaPek <Hgogogo@yandex.ru>
2023-11-22 23:18:01 +0400

    анимации врага, задержка перед нанесением урона

--- nodes/enemy_nodes/Enemy.gd
+++ nodes/enemy_nodes/Enemy.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 var player
+var animation
 var speed = 100
 var delta = 10
 var stop_distance = 200
@@ -11,13 +12,11 @@ var timer
 var can_hit = true
 	
 func _ready():
+	animation = get_node("AnimatedSprite")
 	player = get_parent().get_parent().get_player()
 	add_to_group("Enemies")
 	player.isCombatMode = true
 	hp_bar.value = hp
-	
-	
-	#animation_player = get_node("AnimationPlayer")  # Предполагается, что у вас есть узел AnimationPlayer с именем "AnimationPlayer"
 	
 	timer = Timer.new()
 	add_child(timer)
@@ -25,27 +24,49 @@ func _ready():
 	timer.connect("timeout", self, "_on_Timer_timeout")  # Подключаем сигнал "timeout" к функции _on_Timer_timeout
 	timer.start()  # Запускаем таймер
 	
+	animation.connect("animation_finished", self, "_on_Animation_finished")
+	
 func _process(delta):
 	var direction = (player.position - position).normalized()
 	var distance = position.distance_to(player.position)
 	if distance > stop_distance:
 		position += direction * speed * delta
 		
-		#if direction.x > 0:
-		#	animation_player.play("move_right")  # Предполагается, что у вас есть анимация "move_right"
-		#elif direction.x < 0:
-		#	animation_player.play("move_left")  # Предполагается, что у вас есть анимация "move_left"
-		#elif direction.y > 0:
-		#	animation_player.play("move_down")  # Предполагается, что у вас есть анимация "move_down"
-		#elif direction.y < 0:
-		#	animation_player.play("move_up")  # Предполагается, что у вас есть анимация "move_up"
+		if direction.x > 0:
+			animation.play("walk_right")  # Предполагается, что у вас есть анимация "move_right"
+		elif direction.x < 0:
+			animation.play("walk_left")  # Предполагается, что у вас есть анимация "move_left"
+		elif direction.y > 0:
+			animation.play("walk_down")  # Предполагается, что у вас есть анимация "move_down"
+		elif direction.y < 0:
+			animation.play("walk_up")  # Предполагается, что у вас есть анимация "move_up"
+		
+	#elif :
+	#	# Если враг не двигается, то включаем анимацию покоя
+	#	if direction.x > 0:
+	#		animation.play("idle_right")  # Предполагается, что у вас есть анимация "idle_right"
+	#	elif direction.x < 0:
+	#		animation.play("idle_left")  # Предполагается, что у вас есть анимация "idle_left"
+	#	elif direction.y > 0:
+	#		animation.play("idle_down")  # Предполагается, что у вас есть анимация "idle_down"
+	#	elif direction.y < 0:
+	#		animation.play("idle_up")  # Предполагается, что у вас есть анимация "idle_up"
+			
 	elif distance <= stop_distance and can_hit:
-		player.hit(damage)
-		can_hit = false
-		timer.start()  # Перезапускаем таймер
+		animation.play("attack_left")
+		#player.hit(damage)
+		#can_hit = false
+		#timer.start()  # Перезапускаем таймер
 
 func _on_Timer_timeout():
 	can_hit = true
+
+func _on_Animation_finished():
+	var distance = position.distance_to(player.position)
+	if animation.animation == "attack_left" and can_hit and (distance <= stop_distance):
+		player.hit(damage)
+		can_hit = false
+		timer.start()  # Перезапускаем таймер
 
 func hit(damage_x):
 	hp -= damage_x
